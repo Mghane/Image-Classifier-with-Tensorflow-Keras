8dea51b7e228c2da3d86566c86260c65d311c126
Mehdi Ghane <50430952+Mghane@users.noreply.github.com>
2020-06-18 13:30:37 -0600

    Delete processing.py

--- processing.py
+++
@@ -1,9 +0,0 @@
-# Create the process_image function
-def process_image (image):
-    print('original image shape is:', image.shape)
-    image = tf.cast(image, dtype=tf.float64)
-    image = tf.image.resize(image, (224, 224))
-    image /= 255
-    image = image.numpy()
-    print('Processed image shape is:', image.shape)
-    return image
